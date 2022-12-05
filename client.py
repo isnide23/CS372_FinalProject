ac8237e1cf0d3d7b969a9382b8bbbe6dee5fab6c
Ian Snyder <isnide23@gmail.com>
2022-12-04 17:32:59 -0800

    server can take hello packets

---
+++ client.py
@@ -0,0 +1,92 @@
+# Author: Ian Snyder
+# CS372 Final Project
+import sys
+import socket
+import json
+import time
+import threading
+
+from chatui import init_windows, read_command, print_message, end_windows
+
+nick = sys.argv[1]
+server_addr = sys.argv[2]
+port = int(sys.argv[3])
+
+buffer = b''
+
+hello_dict = {
+    "type": "hello",
+    "nick": nick
+}
+
+chat_dict = {
+    "type": "chat",
+    "message": ""
+}
+
+def decode_packet(packet):
+    return json.loads(packet[2:].decode())
+
+def get_next_packet(s):
+    global buffer
+
+    packet_buffer = buffer
+
+    while True:
+    # if i have complete packet then slice
+        if len(packet_buffer) >= 2:
+            word_length = int.from_bytes(packet_buffer[:2], "big")
+            packet_size = word_length + 2
+            if packet_size <= len(packet_buffer):
+                packet = packet_buffer[:packet_size]
+                packet_buffer = packet_buffer[packet_size:]
+                return packet 
+        chunk = s.recv(5)
+        if len(chunk) == 0:
+            return None
+        packet_buffer += chunk
+
+def build_packet(dict):
+    json_data = json.dumps(dict).encode()
+    packet_len = len(json_data)
+    ready_packet = packet_len.to_bytes(2, "big") + json_data
+    return ready_packet
+
+def runner():
+    global s
+
+    while True:
+        packet = get_next_packet(s)
+        decoded_packet = decode_packet(packet)
+       
+init_windows()
+
+# Make the client socket and connect
+s = socket.socket()
+s.connect((server_addr, port))
+s.sendall(build_packet(hello_dict))
+
+# reciever thread
+t1 = threading.Thread(target=runner, daemon=True)
+t1.start()
+
+# input loop
+while True:
+    try:
+        command = read_command(f"{hello_dict['nick']}> ")
+    except:
+        break
+
+    print_message(f">>> {command}")
+    chat_dict["message"] = command
+    s.sendall(build_packet(chat_dict))
+    packet = decode_packet(get_next_packet(s))
+    if packet['type'] == 'join':
+        print("someone joined")
+    
+
+end_windows()
+
+
+
+
---
+++ server.py
@@ -0,0 +1,112 @@
+# Author: Ian Snyder
+# CS372 Final
+import select
+import socket
+import sys
+import json
+
+packet_dict = {}
+client_dict = {}
+
+def decode_packet(packet):
+    return json.loads(packet[2:].decode())
+
+def build_packet(dict):
+    json_data = json.dumps(dict).encode()
+    packet_len = len(json_data)
+    ready_packet = packet_len.to_bytes(2, "big") + json_data
+    return ready_packet
+
+def get_next_packet(s):
+    packet_buffer = packet_dict[s]
+
+    while True:
+    # if i have complete packet then slice
+        if len(packet_buffer) >= 2:
+            word_length = int.from_bytes(packet_buffer[:2], "big")
+            packet_size = word_length + 2
+            if packet_size <= len(packet_buffer):
+                packet = packet_buffer[:packet_size]
+                packet_buffer = packet_buffer[packet_size:]
+                return packet 
+        chunk = s.recv(5)
+        if len(chunk) == 0:
+            return None
+        packet_buffer += chunk
+
+def run_server(port):
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("", port))
+    s.listen()
+
+    read_set = [s]
+    # main loop:
+    while True:
+    # call select() and get the sockets that are ready to read
+        ready_to_read, _, _ = select.select(read_set, {}, {})
+
+    # for all sockets that are ready to read:
+        for ready_socket in ready_to_read:
+        #   if the socket is the listener socket:
+        #   accept() a new connection
+        #   add the new socket to our set!
+            if ready_socket is s:
+                client_con, client_addr = s.accept()
+                print(f"{client_addr}: connected")
+
+                read_set.append(client_con)
+                packet_dict[client_con] = b''
+        #   else the socket is a regular socket:
+        #   recv() the data from the socket
+            else:
+                packet = get_next_packet(ready_socket)
+        #   if you receive zero bytes
+        #   the client hung up
+        #   remove the socket from the set
+                if not packet:
+                    print(f"{ready_socket.getpeername()}: disconnected")
+
+                    read_set.remove(ready_socket)
+                else:
+                    decoded_packet = decode_packet(packet)
+                    # CASES:
+                    # if hello packet add nick to connected client dictionary
+                    # broadcast a join message to all clients except the sender
+                    if decoded_packet['type'] == 'hello':
+                        client_dict[ready_socket] = decoded_packet['nick']
+                        for k in client_dict:
+                            print(client_dict[k])
+                        join_payload = {"type": "join",
+                        "nick": client_dict[ready_socket]
+                        }
+                        for clients in read_set:
+                            if clients != s: 
+                                clients.sendall(build_packet(join_payload))
+                                print(f"JOIN PAYLOAD: {build_packet(join_payload)}")
+                    
+                    # chat
+                    elif decoded_packet['type'] == 'chat':
+                        
+                        pass 
+                    # join
+                    # disconnect
+                    else:
+                        pass
+                   
+
+
+def usage():
+    print("usage: python chat_server.py 3490", file=sys.stderr)
+
+def main(argv):
+    try:
+        port = int(argv[1])
+    except:
+        usage()
+        return 1
+
+    run_server(port)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
